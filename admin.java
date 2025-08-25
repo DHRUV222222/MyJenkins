08b6f7454531ef11c8755447b2f8c5d8b72d61a8
DHRUV222222 <dhruvshetty2209@gmail.com>
2025-08-25 16:37:33 +0530

    First commit

--- admin.java
+++ admin.java
@@ -1,18 +1,10 @@
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
+        int num1 = 10;   // hardcoded values
+        int num2 = 20;
 
         int sum = num1 + num2;
         System.out.println("Sum = " + sum);
-
-        sc.close();
     }
 }
+
